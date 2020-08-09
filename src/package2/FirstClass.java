e61472f53d354a32f51c26177af048a3c602c872
mudicom52 <68753048+mudicom52@users.noreply.github.com>
2020-08-09 18:41:41 +0300

    Update FirstClass.java
    This is the first changes from remote repo

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
 		System.out.println("This is the first changes from local master");
 		System.out.println("This is the second changes from local master");
 		System.out.println("This is the third changes from local master");
+		System.out.println("This is the first changes from remote repo");
 
 	}
 }
