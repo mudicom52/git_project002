3c854cacf8a6df3ea3c55a95fc5ffdc979534e2d
Muhammet AYDIN <mudicom52@gmail.com>
2020-08-09 18:49:48 +0300

    this is the fourth commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,9 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the first changes from local master");
-		System.out.println("This is the second changes from local master");
-		System.out.println("This is the third changes from local master");
-		System.out.println("This is the first changes from remote repo");
+		System.out.println("This is the second commit from local master");
+		System.out.println("This is the third commit from local master");
+		System.out.println("This is the first commit from remote repo");
+		System.out.println("This is the fourth commit from remote repo");
 
 	}
 }
